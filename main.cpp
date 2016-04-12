4ca416e326b65c8209e571bafbd8b4cb14c92671
joongbu <tmdrbs123@naver.com>
2016-04-11 19:14:03 -0700

    pcap_test7

--- main.cpp
+++ main.cpp
@@ -2,15 +2,10 @@
 #include<stdint.h>
 #include <arpa/inet.h>
 #include <stdio.h>
-
+#include <libnet.h>
+#include <arpa/inet.h>
+#include <netinet/ether.h>
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
-int ip_destination(const u_char *p, int a, int b);
-int ip_source(const u_char *p, int a, int b);
-void ethernet(const u_char *p, int a, int b);
-int tcp_source(const u_char *p);
-int tcp_destination(const u_char *p);
-int udp_source(const u_char *p);
-int udp_destination(const u_char *p);
 
 
 int main()
@@ -21,21 +16,12 @@ int main()
 
     pcap_t *handle;            /* Session handle */
 
+
     char *dev;            /* The device to sniff on */
 
     char errbuf[PCAP_ERRBUF_SIZE];    /* Error string */
-    struct bpf_program fp;        /* The compiled filter */
 
-    char filter_exp[] = "port 23";    /* The filter expression */
-
-    bpf_u_int32 mask;        /* Our netmask */
-
-    bpf_u_int32 net;        /* Our IP */
-
-
-    /* Define the device */
-
-    dev = pcap_lookupdev(errbuf);
+   dev  =  pcap_lookupdev(errbuf);
 
     if (dev == NULL) {
 
@@ -45,21 +31,7 @@ int main()
 
     }
 
-
-    /* Find the properties for the device */
-
-    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-
-        fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
-
-        net = 0;
-
-        mask = 0;
-
-    }
-
     /* Open the session in promiscuous mode */
-
     handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 
     if (handle == NULL) {
@@ -69,17 +41,6 @@ int main()
         return(2);
 
     }
-
-    /* Compile and apply the filter */
-
-    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-
-        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-
-        return(2);
-
-    }
-
 
     pcap_loop(handle, -1, got_packet, NULL);
 
@@ -93,113 +54,30 @@ int main()
 }
 
 
-void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *ethernet_header) {
-    const u_char *ip_header = ethernet_header + 14;
-    const u_char *tcp_header = ip_header + (ip_header[0] & 0x0f) * 4;
-    const u_char *udp_header = ip_header + (ip_header[0] & 0x0f) * 4;
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+    libnet_ethernet_hdr *ethernet_header = (libnet_ethernet_hdr*) packet;
 
-    ethernet(ethernet_header,0,11);
-    if((int)ethernet_header[12] == 8 && (int)ethernet_header[13] == 0)
-    {
-        ip_source(ip_header,12,15);
-        ip_destination(ip_header,16,19);
-        if((int)ethernet_header[23] == 6)
+        printf("destination MAC : %s\n",ether_ntoa((ether_addr*)(ethernet_header->ether_dhost)));
+        printf("source MAC : %s\n",ether_ntoa((ether_addr*)(ethernet_header->ether_shost)));
+        if(ntohs(ethernet_header->ether_type) == ETHERTYPE_IP)
         {
+            libnet_ipv4_hdr *ip_header = (libnet_ipv4_hdr *)(packet + sizeof(libnet_ethernet_hdr));
+            printf("ip source : %s\n",inet_ntoa(ip_header->ip_src));
+            printf("ip destination : %s\n",inet_ntoa(ip_header->ip_dst));
 
-           tcp_source(tcp_header);
-           tcp_destination(tcp_header);
+            if(ip_header->ip_p == IPPROTO_TCP) //0x06
+            {
+                libnet_tcp_hdr *tcp_header = (libnet_tcp_hdr *)(packet + sizeof(libnet_ethernet_hdr) + (ip_header->ip_hl*4));
+                printf("tcp source port : %d\n", ntohs(tcp_header->th_sport));
+                printf("tcp destination port : %d\n",ntohs(tcp_header->th_dport));
+
+            }
+            if(ip_header->ip_p == IPPROTO_UDP)//0x11
+            {
+                libnet_udp_hdr *udp_header = (libnet_udp_hdr *)(packet + sizeof(libnet_ethernet_hdr) + (ip_header->ip_hl*4));
+                printf("udp source port : %d\n", ntohs(udp_header->uh_sport));
+                printf("udp destination port : %d\n",ntohs(udp_header->uh_dport));
+            }
+
         }
-       if((int)ethernet_header[23] == 17)
-       {
-
-           udp_source(udp_header);
-           udp_destination(udp_header);
-
-       }
-
-    }
-
-
 }
-void ethernet(const u_char *p, int a, int b)
-{
-    int i = 0;
-    printf("ethernet destination :");
-    for(i = a ; i<=b/2; i = a++)
-    {
-        printf("%02x",p[a]);
-        if(a<b/2)
-        printf(":");
-    }
-    printf("\n");
-    printf("ethernet source :");
-    for(i=b/2;i <=b;i = a++)
-    {
-        printf("%02x",p[a]);
-        if(a<b)
-        printf(":");
-    }
-    printf("\n");
-}
-
-int ip_destination(const u_char *p, int a, int b)
-{
-    int i = 0;
-
-    printf("ip destination : ");
-    for(i= a; i<=b; i = a++)
-    {
-        printf("%02d",p[a]);
-        if(a<b)
-            printf(".");
-    }
-    printf("\n");
-    return 0;
-}
-
-int ip_source(const u_char *p, int a, int b)
-{
-    int i = 0;
-    printf("ip source : ");
-    for(i = a; i<=b; i = a++)
-    {
-        printf("%02d",p[a]);
-        if(a<b)
-            printf(".");
-    }
-    printf("\n");
-    return 0;
-}
-
-int tcp_destination(const u_char *p)
-{
-    uint8_t buffer[2] = {p[2],p[3]};
-    uint16_t *s = (uint16_t*) buffer;
-    printf("tcp destination port : %d\n",ntohs(*s));
-    return 0;
-}
-
-int tcp_source(const u_char *p)
-{
-    uint8_t buffer[2]= {p[0],p[1]};
-    uint16_t *s = (uint16_t*) buffer;
-    printf("tcp source port : %d\n",ntohs(*s));
-    return 0;
-}
-
-int udp_destination(const u_char *p)
-{
-    uint8_t buffer[2] = {p[2],p[3]};
-    uint16_t *s = (uint16_t*) buffer;
-    printf("udp destination port : %d\n",ntohs(*s));
-    return 0;
-}
-
-int udp_source(const u_char *p)
-{
-    uint8_t buffer[2]= {p[0], p[1]};
-    uint16_t *s = (uint16_t*) buffer;
-    printf("udp source port : %d\n",ntohs(*s));
-    return 0;
-}
-
--- pcap_test.pro
+++ pcap_test.pro
@@ -4,3 +4,10 @@ CONFIG -= app_bundle
 CONFIG -= qt
 LIBS += -lpcap
 SOURCES += main.cpp
+HEADERS += \
+    libnet/libnet-headers.h \
+    libnet/libnet-macros.h \
+    libnet/libnet-structures.h \
+    libnet/libnet-types.h \
+    libnet/libnet-asn1.h \
+    libnet/libnet-functions.h
